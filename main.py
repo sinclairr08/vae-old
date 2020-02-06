12b9afac8c201afd3af1e5f89c6ac202482b3009
Lee Jun Hyuk <38907328+PengSasm@users.noreply.github.com>
2020-02-06 18:13:46 +0900

    #commit 4
    Add AAE model, fix some bugs on LSTM VAE but still some problem

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ import torch.utils.data
 from torch import optim
 from torchvision import datasets, transforms
 
-from models import VAE
-from models import LSTM_VAE
+from models import VAE, LSTM_VAE, AAE
 
 from utils import to_gpu, batchify
 from preprocess import Corpus
@@ -33,8 +32,8 @@ def main(args):
         )
 
         Model = VAE(nlatent=args.nlatent,
-                    ninput=args.ninput, #com2
-                    nhidden=args.nhidden, # com2
+                    ninput=args.ninput,
+                    nhidden=args.nhidden,
                     is_gpu = args.cuda)
         Model = to_gpu(Model, args.cuda)
         optimizer = optim.Adam(Model.parameters(), lr=args.lr)
@@ -70,8 +69,38 @@ def main(args):
             Model.train_epoch(epoch, optimizer, train_loader, args.log_file, args.log_interval)
             Model.test_epoch(epoch, test_loader, corpus.dictionary.idx2word, args.log_file,
                              args.save_path)
-            Model.sample(epoch, sample_num=args.sample_num, maxlen = args.maxlen, save_path=args.save_path,
-                         sample_method = 'sampling')
+            Model.sample(epoch, sample_num=args.sample_num, maxlen = args.maxlen, idx2word = corpus.dictionary.idx2word,
+                         save_path=args.save_path, sample_method = 'sampling')
+            # mod : NAN error
+            # moded : error fix
+
+    # Case 3 : MNIST with AAE (Need more automization)
+    if args.dataset == 'mnist' and args.model == 'aae':
+        kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}  # mod : Is it really need?
+
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('./data', train = True, download=True,
+                           transform = transforms.ToTensor()),
+            batch_size=args.batch_size, shuffle = True, **kwargs
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('./data', train=False, download=True,
+                           transform=transforms.ToTensor()),
+            batch_size=args.batch_size, shuffle=False, ** kwargs
+        )
+
+        Model = AAE(nlatent=args.nlatent,
+                    ninput=args.ninput,
+                    nhidden=args.nhidden,
+                    is_gpu = args.cuda)
+        Model = to_gpu(Model, args.cuda)
+
+        for epoch in range(1, args.epochs + 1):
+            Model.train_epoch(epoch, train_loader, args.log_file)
+            Model.test_epoch(epoch, test_loader, args.log_file)
+            Model.sample(epoch, sample_num=args.sample_num, save_path=args.save_path)
+
 
 
 if __name__ == "__main__":
@@ -84,23 +113,23 @@ if __name__ == "__main__":
     parser.add_argument('--sample_num', type=int, default=20, help='The number of samples')
 
     # Data & Model Arguments
-    parser.add_argument('--dataset', type=str, default='snli', help='dataset; [mnist, snli]')
-    parser.add_argument('--model', type=str, default='lstmvae', help='model; [vae, lstmvae]') # com2
-    parser.add_argument('--maxlen', type=int, default=30, help='Max length of the sentence; Exceeded words are truncated') # com2
+    parser.add_argument('--dataset', type=str, default='mnist', help='dataset; [mnist, snli]')
+    parser.add_argument('--model', type=str, default='aae', help='model; [vae, lstmvae, aae]')
+    parser.add_argument('--maxlen', type=int, default=30, help='Max length of the sentence; Exceeded words are truncated')
 
     # Model Architecture Arguments
-    parser.add_argument('--ninput', type=int, default=784, help='The dimension size of input') #com2
-    parser.add_argument('--nemb', type=int, default=300, help='The dimension size of embedding')  # com2
+    parser.add_argument('--ninput', type=int, default=784, help='The dimension size of input')
+    parser.add_argument('--nemb', type=int, default=300, help='The dimension size of embedding')
     parser.add_argument('--nlatent', type=int, default=300, help='The dimension size of latent')
     parser.add_argument('--nlayers', type=int, default=1, help='The number of layers')
-    parser.add_argument('--nhidden', type=int, default=300, help='The hidden dimension size of LSTM or CNN') #com2
-    parser.add_argument('--nvocab', type=int, default=20000, help='The number of vocabulary yo use')  # com2
+    parser.add_argument('--nhidden', type=int, default=300, help='The hidden dimension size of LSTM or CNN')
+    parser.add_argument('--nvocab', type=int, default=20000, help='The number of vocabulary to use')
     # Can add - encoder arch, dec arch,
 
     # Training Arguments
-    parser.add_argument('--epochs', type=int, default=20, help='The maximum number of epochs')
+    parser.add_argument('--epochs', type=int, default=10, help='The maximum number of epochs')
     parser.add_argument('--batch_size', type=int, default=64, help='The number of batch size')
-    parser.add_argument('--lr', type=float, default=1e-01, help='Learning rate of ##') # Need separation
+    parser.add_argument('--lr', type=float, default=1, help='Learning rate of ##') # Need separation.. and modification
     parser.add_argument('--anneal_function', type=str, default='logistic', help='kl annealing function; [logistic]') # Add other functions
 
     # File load & Save  Arguments
--- models.py
+++ models.py
@@ -20,8 +20,8 @@ class VAE(nn.Module):
         # self init area
         self.is_gpu = is_gpu
         self.nlatent = nlatent
-        self.ninput = ninput #com2
-        self.nhidden = nhidden #com2
+        self.ninput = ninput
+        self.nhidden = nhidden
 
         # mod : Modlarize within the class
         #self.enc = None
@@ -37,32 +37,22 @@ class VAE(nn.Module):
     # mod : Modularization
     # mod : layer of mu, logvar
     def encode(self, input):
-        """
-        if self.enc is None:
-            raise NotImplementedError
-        return self.enc(input)
-        """
-        h1 = F.relu(self.enc1(input))
+        h1 = F.relu(self.enc1(input.view(-1, self.ninput)))
         return self.enc21(h1), self.enc22(h1)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
-        eps = to_gpu(torch.rand_like(self.std), self.is_gpu) # epsilon for reparametrization
+        eps = to_gpu(torch.rand_like(std), self.is_gpu) # epsilon for reparametrization
         return mu + eps*std
 
     # mod : Modularization
     def decode(self, latent):
-        """
-        if self.dec is None:
-            raise NotImplementedError
-        return self.dec(latent)
-        """
         h2 = F.relu(self.dec1(latent))
         return torch.sigmoid(self.dec2(h2))
 
     # Returns the input to the output, mu, and log var
     def forward(self, input):
-        mu, logvar = self.encode(input.view(-1, self.ninput))
+        mu, logvar = self.encode(input)
         latent = self.reparameterize(mu, logvar)
         output = self.decode(latent)
 
@@ -380,8 +370,16 @@ class LSTM_VAE(nn.Module):
             token_logits = self.hidden2token(output.squeeze(1))
 
             if sample_method == 'sampling':
+                # print(token_logits)
                 token_probs = F.softmax(token_logits, dim = -1)         # review that why it is -1
-                token_indicies = torch.multinomial(token_probs, num_samples=1)
+                # print(token_probs)
+
+                # mod : Error that negative prob
+                try:
+                    token_indicies = torch.multinomial(token_probs, num_samples=1)
+                except:
+                    print(token_probs)
+                    exit(-1)
 
             elif sample_method == 'greedy':
                 token_indicies = torch.argmax(token_logits, dim = 1)
@@ -397,14 +395,21 @@ class LSTM_VAE(nn.Module):
             embs = embs.squeeze(1)
             aug_embs = torch.cat([embs, random_noise.unsqueeze(1)], 2)
 
+
         cat_token_indicies = torch.cat(all_token_indicies, 1)
+
+        #print(cat_token_indicies.shape)
         cat_token_indicies = cat_token_indicies.squeeze(2)
         cat_token_indicies = cat_token_indicies.data.cpu().numpy()
+
+        print(cat_token_indicies.shape)
+        input()
 
         sampling_file = os.path.join(save_path, 'epoch_' + str(epoch) + "_sampling.txt")
         sentences = []
         for idx in cat_token_indicies:
             words = [idx2word[x] for x in idx]
+
             sentence_list = []
 
             for word in words:
@@ -420,6 +425,164 @@ class LSTM_VAE(nn.Module):
 
         return
 
+class AAE(nn.Module):
+    def __init__(self, nlatent, ninput, nhidden,  is_gpu):
+
+        super(AAE, self).__init__()
+        # self init area
+        self.is_gpu = is_gpu
+        self.nlatent = nlatent
+        self.ninput = ninput
+        self.nhidden = nhidden
+
+        # mod : Modlarize within the class
+        #self.enc = None
+        #self.dec = None
+
+        # Encoder
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(self.ninput, self.nhidden),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self.nhidden, self.nlatent),
+        )
+
+        # Decoder
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(self.nlatent, self.nhidden),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self.nhidden, self.ninput),
+            torch.nn.Sigmoid()
+        )
+
+        # Discriminator
+        self.disc = torch.nn.Sequential(
+            torch.nn.Linear(self.nlatent, self.nhidden),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self.nhidden, 1),
+            torch.nn.Sigmoid()
+        )
+
+        # mod : Total Optim requires somewhat change
+        self.optim_enc_nll = torch.optim.Adam(self.encoder.parameters(), lr=1e-04)
+        self.optim_enc_adv = torch.optim.Adam(self.encoder.parameters(), lr=5e-05)
+        self.optim_dec = torch.optim.Adam(self.decoder.parameters(), lr=1e-04)
+        self.optim_disc = torch.optim.Adam(self.encoder.parameters(), lr=5e-05)
+
+        # Epsilon to prevent 0
+        self.eps = 1e-15
+
+
+    # mod : Check that is it really needs
+    def encode(self, input):
+        return self.encoder(input.view(-1, self.ninput))
+
+    # mod : Check that is it really needs
+    def decode(self, latent):
+        return self.decoder(latent)
+
+    # Returns the latent an doutput
+    def forward(self, input):
+        latent = self.encode(input)
+        output = self.decode(latent)
+
+        return output, latent
+
+    def nll_loss(self, input, output):
+        loss_fn = nn.BCEWithLogitsLoss(reduction='sum')
+        loss = loss_fn(output, input.view(-1, self.ninput))
+
+        return loss
+
+    def disc_loss(self, latent_real, latent_fake):
+        assert latent_real.shape == latent_fake.shape
+
+        disc_real = self.disc(latent_real)
+        disc_fake = self.disc(latent_fake)
+
+        loss = -torch.mean(torch.log(disc_real + self.eps) + torch.log(1 - disc_fake + self.eps))
+
+        return loss
+
+    def adv_loss(self, input):
+        latent_fake = self.encode(input)
+        disc_fake = self.disc(latent_fake)
+
+        loss = -torch.mean(torch.log(disc_fake + self.eps))
+        return loss
+
+    def train_epoch(self, epoch, train_loader, log_file):
+        self.train()
+        self.optim_enc_nll.zero_grad()
+        self.optim_enc_adv.zero_grad()
+        self.optim_dec.zero_grad()
+        self.optim_disc.zero_grad()
+
+        total_nll_loss = 0
+        total_adv_loss = 0
+
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data = to_gpu(data, self.is_gpu)
+
+            # Phase 1 : Train Autoencoder
+            output, _ = self.forward(data)
+
+            nll_loss = self.nll_loss(data, output)            # Need to check the names
+            nll_loss.backward()
+            total_nll_loss += nll_loss.item()
+            self.optim_enc_nll.step()
+            self.optim_dec.step()
+
+            # Phase 2 : Train Discriminator
+
+            latent_fake = self.encode(data)
+            latent_real = to_gpu(Variable(torch.randn_like(latent_fake)), self.is_gpu)
+
+            disc_loss = self.disc_loss(latent_real, latent_fake)
+            disc_loss.backward()
+
+            self.optim_disc.step()
+
+            # Phase 3 : Train encoder
+            self.encoder.train()
+            adv_loss = self.adv_loss(data)
+            adv_loss.backward()
+            total_adv_loss += adv_loss.item()
+            self.optim_enc_adv.step()
+
+        total_loss = total_nll_loss + total_adv_loss
+        len_data = len(train_loader.dataset)
+        log_line("Epoch {} Train Loss : {:.4f} NLL Loss : {:.4f} Adv Loss : {:.4f}".format(
+            epoch, total_loss / len_data, total_nll_loss / len_data,
+            total_adv_loss / len_data), log_file, is_print=True)
+
+    def test_epoch(self, epoch, test_loader, log_file):
+        self.eval()
+        total_nll_loss = 0
+        total_adv_loss = 0
+
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(test_loader):
+                data = to_gpu(data, self.is_gpu)
+                output, _ = self.forward(data)
+
+                nll_loss = self.nll_loss(data, output)
+                total_nll_loss += nll_loss.item()
+
+                adv_loss = self.adv_loss(data)
+                total_adv_loss += adv_loss.item()
+
+        total_loss = total_nll_loss + total_adv_loss
+        len_data = len(test_loader.dataset)
+        log_line("Epoch {} Test Loss : {:.4f} NLL Loss : {:.4f} Adv Loss : {:.4f}".format(
+            epoch, total_loss / len_data, total_nll_loss / len_data,
+                    total_adv_loss / len_data), log_file, is_print=True)
+
+    def sample(self, epoch, sample_num, save_path):
+        with torch.no_grad():
+            random_noise = to_gpu(torch.randn(sample_num, self.nlatent), self.is_gpu)
+            sample = self.decode(random_noise).cpu()
+            save_image(sample.view(sample_num, 1, 28, 28),
+                       save_path + 'epoch_' + str(epoch) + '.png')
 
 
 
